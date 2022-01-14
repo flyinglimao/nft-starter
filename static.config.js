56ad98dba072194eae156c24b20165f741ee3578
flyinglimao <hi@limaois.me>
2022-01-15 02:10:28 +0800

    feat: setup styles

--- static.config.js
+++ static.config.js
@@ -1,31 +1,12 @@
-import axios from 'axios'
-import path from 'path'
+import path from 'path';
+import React from 'react';
+
 // import { Post } from './types'
 
 // Typescript support in static.config.js is not yet supported, but is coming in a future update!
 
 export default {
   entry: path.join(__dirname, 'src', 'index.tsx'),
-  getRoutes: async () => {
-    const { data: posts } /* :{ data: Post[] } */ = await axios.get(
-      'https://jsonplaceholder.typicode.com/posts'
-    )
-    return [
-      {
-        path: '/blog',
-        getData: () => ({
-          posts,
-        }),
-        children: posts.map((post /* : Post */) => ({
-          path: `/post/${post.id}`,
-          template: 'src/containers/Post',
-          getData: () => ({
-            post,
-          }),
-        })),
-      },
-    ]
-  },
   plugins: [
     'react-static-plugin-typescript',
     [
@@ -36,5 +17,29 @@ export default {
     ],
     require.resolve('react-static-plugin-reach-router'),
     require.resolve('react-static-plugin-sitemap'),
+    'react-static-plugin-emotion',
   ],
-}
+  Document: ({
+    Html,
+    Head,
+    Body,
+    children,
+    state: { siteData, renderMeta },
+  }) => (
+    <Html lang="en-US">
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+      </Head>
+      <Body>{children}</Body>
+    </Html>
+  ),
+};
